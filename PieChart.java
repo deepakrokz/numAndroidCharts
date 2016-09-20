4b0ca321f6362c44a92125751ab51e73b1b5fc0a
deepakrokz <deepak.gvu@gmail.com>
2016-09-20 12:38:50 +0530

    Pie Chart custom color
    Here user can add his/her own custom not random color

---
+++ PieChart.java
@@ -0,0 +1,135 @@
+package com.wedigtech.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.util.List;
+
+/**
+ * Created by USER on 9/19/2016.
+ */
+public class PieChart extends View {
+
+
+    List<ChartData> data;
+    Paint piePaint;
+    RectF mBounds;
+    int width;
+    int height;
+
+
+    public PieChart(Context context, AttributeSet attributeSet){
+        super(context, attributeSet);
+
+        init();
+    }
+
+
+    public void init(){
+
+        piePaint = new Paint();
+
+        piePaint.setAntiAlias(true);
+        piePaint.setDither(true);
+        piePaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Try for a width based on our minimum
+        int minw = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
+
+        width = Math.max(minw, MeasureSpec.getSize(widthMeasureSpec));
+
+        // Whatever the width ends up being, ask for a height that would let the pie
+        // get as big as it can
+        int minh = width  + getPaddingBottom() + getPaddingTop();
+        height = Math.min(MeasureSpec.getSize(heightMeasureSpec), minh);
+        setMeasuredDimension(width, height);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        if (data != null){
+
+            int top = 10;
+            int left = 0;
+            int endBottom = getHeight();
+            int endRight = endBottom;
+
+
+            mBounds = new RectF(left, top, endRight, endBottom);
+
+            float[] segment = pieSegment();
+            int[] color_ar = getColor();
+
+            float segStartPoint = 0;
+
+            for (int i = 0; i < color_ar.length; i++){
+
+//                Random rnd = new Random();
+//                int color = Color.argb(255, (int) segment[i], rnd.nextInt(256), rnd.nextInt(256));
+
+                piePaint.setColor(color_ar[i]);
+                canvas.drawArc(mBounds, segStartPoint, segment[i], true, piePaint);
+                segStartPoint += segment[i];
+            }
+        }
+        else {
+            piePaint.setColor(Color.BLUE);
+            canvas.drawOval(mBounds, piePaint);
+        }
+    }
+
+
+    private float[] pieSegment(){
+
+        float[] segValues = new float[this.data.size()];
+        float Total = getTotal();
+
+        for (int i = 0; i < this.data.size(); i++){
+
+            segValues[i] = (this.data.get(i).getValue()/Total) * 360;
+        }
+
+        return segValues;
+    }
+
+    private int[] getColor(){
+        int[] colorValues = new int[this.data.size()];
+        for (int i = 0; i < this.data.size(); i++) {
+            colorValues[i] = this.data.get(i).getColor();
+        }
+        return colorValues;
+    }
+
+
+    private float getTotal(){
+
+        Log.e("data string", this.data.size()+"");
+
+        float total = 0;
+
+        for(int i = 0; i < this.data.size(); i++){
+
+            total += this.data.get(i).getValue();
+        }
+        return total;
+    }
+
+
+    public void setData(List<ChartData> data){
+
+        this.data = data;
+        invalidate();
+    }
+
+}
